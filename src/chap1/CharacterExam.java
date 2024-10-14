3a6ddc0bde33275c2ebd73dc57e1caff972d9475
jiny3360 <jiny3360@naver.com>
2024-10-14 11:24:10 +0900

    20241014 - #1 눈물의 예시코드 다시 작성 중...

---
+++ src/chap1/CharacterExam.java
@@ -0,0 +1,18 @@
+package chap1;
+
+public class CharacterExam {
+    public static void main(String[] args) {
+        char char1 = 'A';
+
+        System.out.println(char1);
+        System.out.println((int)char1);
+        char char2 = 66;
+
+        System.out.println(char2);
+        System.out.println((int)char2);
+        char char3 = 67;
+
+        System.out.println(char3);
+        System.out.println((char)char3);
+    }
+}
